bef8128c7c371a652b3c9d741159f298a4ff878d
natalia-hnzn <natalia.hnzn@gmail.com>
2023-10-24 21:47:24 -0300

    replace models, add repositories

--- infra/config/connection.py
+++ infra/config/connection.py
@@ -5,7 +5,7 @@ from infra.config.base import Base
 
 class DBConnectionHandler:
     def __init__(self):
-        self.__connection_string = 'sqlite:///embrestimo.db'
+        self.__connection_string = 'sqlite:///emprestimo.db'
         self.__engine = self.__create_database_engine()
         self.create_table()
         self.session = None
---
+++ infra/repository/uniforme_repository.py
@@ -0,0 +1,53 @@
+from infra.config.connection import DBConnectionHandler
+from infra.entities.uniforme import Uniforme
+
+
+class UniformeRepository:
+
+    @staticmethod
+    def select_uniforme_by_id(id_uniforme):
+        with DBConnectionHandler as db:
+            return db.session.query(Uniforme).filter(Uniforme.id == id_uniforme).first()
+
+    @staticmethod
+    def select_uniforme_by_name(name_uniforme):
+        with DBConnectionHandler as db:
+            return db.session.query(Uniforme).filter(Uniforme.nome == name_uniforme).first()
+
+    @staticmethod
+    def select_all_uniformes():
+        with DBConnectionHandler as db:
+            return db.session.query(Uniforme).all()
+
+    @staticmethod
+    def select_first_uniforme():
+        with DBConnectionHandler as db:
+            return db.session.query(Uniforme).first()
+
+    @staticmethod
+    def insert_many_uniformes(uniformes):
+        with DBConnectionHandler as db:
+            if isinstance(uniformes, list):
+                db.session.query(Uniforme).add_all(uniformes)
+                db.session.commit()
+            else:
+                print(uniformes)
+
+    @staticmethod
+    def insert_one_uniforme(uniforme):
+        with DBConnectionHandler as db:
+            db.session.query(Uniforme).add(uniforme)
+            db.session.commit()
+
+    @staticmethod
+    def update_uniforme(uniforme):
+        with DBConnectionHandler as db:
+            db.session.query(Uniforme).filter(Uniforme.id == uniforme.id).update({'nome': uniforme.nome})
+            db.session.commit()
+
+    @staticmethod
+    def delete_uniforme(uniforme):
+        with DBConnectionHandler as db:
+            db.session.query(Uniforme).filter(Uniforme.id == uniforme.id).update({'ativo': False})
+            db.session.commit()
+
